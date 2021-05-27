052ca378473adbe82be7d8ce61726af65071b391
Anna Lisiecka <44555028+anialisiecka@users.noreply.github.com>
2021-05-27 10:08:49 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+rom setuptools import setup
+
+setup(name='alibi',
+      version='0.1',
+      description='',
+      author='Anna Lisiecka',
+      url='https://github.com/anialisiecka/ALIBI',
+      packages=['alibi'],
+      #install_requires=[]
+     )
