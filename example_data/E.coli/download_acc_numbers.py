8156be25f4008759bc14378177c3e91a497e4423
Anna Lisiecka <44555028+anialisiecka@users.noreply.github.com>
2021-06-12 08:47:54 +0200

    Create download_acc_numbers.py

---
+++ example_data/E.coli/download_acc_numbers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+from Bio import Entrez, SeqIO
+
+arguments = sys.argv
+
+Entrez.email = arguments[1]
+file_name = arguments[2]
+
+input_file = open(file_name,"r")
+output_file = open(os.path.splitext(file_name)[0]+".fa","w")
+acc = ""
+
+for line in input_file:
+        line = line.strip()
+        acc += line
+        acc += ","
+acc = acc[:-1]
+
+input_file.close()
+
+handle = Entrez.efetch(db="nucleotide", id=acc, rettype="fasta", retmode="text")
+records = SeqIO.parse(handle, "fasta")
+
+SeqIO.write(records,output_file,"fasta")
+
+handle.close()
+output_file.close()
