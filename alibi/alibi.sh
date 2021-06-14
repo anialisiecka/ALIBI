f67c73a9e9ad2265090959a142510626a7286a60
Anna Lisiecka <44555028+anialisiecka@users.noreply.github.com>
2021-06-14 06:40:11 +0200

    Update alibi.sh

--- alibi/alibi.sh
+++ alibi/alibi.sh
@@ -6,27 +6,14 @@ while test $# -gt 0; do
       echo " "
       echo "-h, --help                          show brief help"
       echo "-i                                  specify an input file to use"
-      echo "-o                                  specify a file to store output in"
       exit 0
       ;;
     -i)
       shift
       if test $# -gt 0; then
         export INPUT=$1
-        echo "$INPUT"
       else
         echo "no input file specified"
-        exit 1
-      fi
-      shift
-      ;;
-    -o)
-      shift
-      if test $# -gt 0; then
-        export OUTPUT=$1
-        echo "$OUTPUT"
-      else
-        echo "no output file specified"
         exit 1
       fi
       shift
@@ -38,14 +25,4 @@ while test $# -gt 0; do
   esac
 done
 
-if [ -z $INPUT ]; then
-  echo "no input file specified"
-  exit 1
-fi
-
-if [ -z $OUTPUT ]; then
-  echo "no output file specified"
-  exit 1
-fi
-
-python gfaSorter.py $INPUT $OUTPUT
+python gfaSorter.py $INPUT
