ed0241a7342f80476170dc734fceaeb7992e8e8c
Anna Lisiecka <44555028+anialisiecka@users.noreply.github.com>
2021-06-14 07:28:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='alibi',
       version='0.1',
       description='Algorithm for Linearization by Incremental graph BuIlding',
       author='Anna Lisiecka',
       url='https://github.com/anialisiecka/ALIBI',
-      packages=['alibi'],
+      packages=find_packages(),
       #install_requires=[]
      )
