bdc3b24645612f9b1e381e7174d66a24f21f08ef
Anna Lisiecka <44555028+anialisiecka@users.noreply.github.com>
2022-05-31 09:42:38 +0200

    Update block.py

--- src/block.py
+++ src/block.py
@@ -7,6 +7,7 @@ class Block:
         self.reorder_shift = 0 # modification caused by edges inserted within group
         self.orient = 1 # orientation relative to parent
         self.flanks = {-1: 0, 1: 0} # blocks in group with negative/positive order (only in root)
+        self.component = set([block_id])
 
     def find(self):
         if self.toroot is self:
@@ -51,7 +52,37 @@ class Block:
         selfroot.shift = flank*(otheroot.flanks[flank]+selfroot.flanks[-reverse*flank]+1)
         otheroot.flanks[flank] += selfroot.flanks[-1]+selfroot.flanks[1]+1
         selfroot.toroot = otheroot
-        del selfroot.flanks
+        otheroot.component.update(selfroot.component)
+        del selfroot.flanks, selfroot.component
+        
+    def uniontoMidst(self, other, reverse, flank, blocks):
+        selfroot = self.find()
+        otheroot = other.find()
+        n = other.order()
+        k = selfroot.size()
+        m = otheroot.order()
+
+        if flank == 1:
+            for i in otheroot.component:
+                if blocks[i].order() > n:
+                    blocks[i].reorder_shift += k
+        else:
+            for i in otheroot.component:
+                if blocks[i].order() < n:
+                    blocks[i].reorder_shift -= k
+
+        selfroot.orient = reverse
+        selfroot.reorder_shift *= reverse
+        selfroot.shift = n + flank*(selfroot.flanks[-reverse*flank] + 1)
+
+        if flank == 1:
+            otheroot.flanks[1] += k
+        else:
+            otheroot.flanks[-1] += k
+
+        selfroot.toroot = otheroot
+        otheroot.component.update(selfroot.component)
+        del selfroot.flanks, selfroot.component
         
     def orient_maf_block(self):
         # Modify alignment according to block orientation
